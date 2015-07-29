8b8c11125cf24c9754d23e779e8f5cbe5fca25c3
eastcoastsb <ryanmcgovern@Ryans-MacBook-Air.local>
2015-07-29 01:27:04 -0400

    renders hexes, needs styling

--- app.js
+++ app.js
@@ -8,5 +8,54 @@
     return;
   };
 
+  var Palette = function() {
+    this.dominant = null;
+    this.contrastingDominant = null;
+    this.subDominant = null;
+    this.contrastingSubDominant = null;
+    this.pop = null;
+  };
+
+  var artsyThings = [
+    "dominant",
+    "contrastingDominant",
+    "subDominant",
+    "contrastingSubDominant",
+    "pop"
+  ];
+
+  var renderPalettes = function(e) {
+    var palettes = JSON.parse(e.target.responseText);
+    var allPalettes = document.createElement('div');
+    allPalettes.className = "palettes__group";
+
+    palettes.forEach(function(palette){
+      var paletteDiv = document.createElement('div');
+      paletteDiv.className = "palette__section";
+      var title = document.createElement('h3');
+      title.className = "palette__title";
+      title.innerText = palette.title;
+      paletteDiv.appendChild(title);
+      var keyword = document.createElement('p');
+      keyword.className = "palette__keyword";
+      keyword.innerText = palette.keyword;
+      paletteDiv.appendChild(keyword);
+      allPalettes.appendChild(paletteDiv);
+
+      var p = new Palette();
+      
+      for(i = 0; i < artsyThings.length; i++) {
+        p = palette.colors[artsyThings[i]];
+        var hexDiv = document.createElement('div');
+        hexDiv.className = 'palette__hex';
+        hexDiv.style.background = p;
+        // hexDiv.innerHTML = '&nbsp;'
+        paletteDiv.appendChild(hexDiv);
+      };
+    });
+    document.body.insertBefore(allPalettes, document.body.childNodes[0]);
+  };
+
   getJSON('/palettes.json', renderPalettes);
+
 })();
